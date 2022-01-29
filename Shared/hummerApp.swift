81b190079104746f34d6f4f15b0d18cce86399f2
amitlvs <amit007.mail2@gmail.com>
2022-01-29 16:27:56 +0530

    Initial Commit

---
+++ Shared/hummerApp.swift
@@ -0,0 +1,17 @@
+//
+//  hummerApp.swift
+//  Shared
+//
+//  Created by AMIT THAKUR on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct hummerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
